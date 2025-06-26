a0361b821a640f2346e4974f16a6a169c63185ae
Bashar ElHusseiny <basharelhusseiny@gmail.com>
2025-06-26 22:19:26 +0300

    image last up

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // تكوين الصور
   images: {
-    domains: [],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-    path: "/_next/image",
     loader: "default",
+    path: "/_next/image",
   },
 };
 
